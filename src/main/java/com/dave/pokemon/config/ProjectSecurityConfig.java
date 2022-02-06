199b01c4d2e993a7c8667d5cc114cec35ac868ac
Dave Harms <daveharmswebdev@gmail.com>
2022-02-06 12:47:18 -0600

    created cors policy for localhost

--- src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
+++ src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
@@ -8,8 +8,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import java.util.Collections;
 
 
 /**
@@ -23,6 +27,18 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }).and()
                 .authorizeRequests()
                     .antMatchers("/api/v1/pokemon").authenticated()
                     .antMatchers("/api/v1/trainer").authenticated()
