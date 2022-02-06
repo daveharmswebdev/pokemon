0351adc87f3fc4c284a1ac328510a476ee1bf773
Dave Harms <daveharmswebdev@gmail.com>
2022-02-05 20:54:48 -0600

    added bcrypt

--- src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
+++ src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
@@ -1,8 +1,16 @@
 package com.dave.pokemon.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
 
 /**
  * User: Dave Harms
@@ -22,4 +30,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin().and().httpBasic();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
