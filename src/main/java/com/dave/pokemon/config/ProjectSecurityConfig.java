bb9e07a9d09ca6a3e158d828c9ea567207fab4d4
Dave Harms <daveharmswebdev@gmail.com>
2022-02-09 13:34:06 -0600

    added jwts

--- src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
+++ src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
@@ -1,17 +1,21 @@
 package com.dave.pokemon.config;
 
+import com.dave.pokemon.filter.JWTTokenGeneratorFilter;
+import com.dave.pokemon.filter.JWTTokenValidatorFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -25,6 +29,8 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .cors().configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
@@ -33,13 +39,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(List.of("Authorization"));
                         config.setMaxAge(3600L);
                         return config;
                     }
                 }).and()
                 .csrf()
-//                .disable()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                    .disable()
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
                     .antMatchers("/api/v1/pokemon").authenticated()
                     .antMatchers("/api/v1/trainer").authenticated()
