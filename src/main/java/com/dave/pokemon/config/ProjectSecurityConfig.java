bc161c1387971750f2c3893f866b8af402cacaa0
Dave Harms <daveharmswebdev@gmail.com>
2022-02-12 08:53:40 -0600

    oauth where client is a service

--- src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
+++ src/main/java/com/dave/pokemon/config/ProjectSecurityConfig.java
@@ -1,20 +1,11 @@
 package com.dave.pokemon.config;
 
-import com.dave.pokemon.filter.JWTTokenGeneratorFilter;
-import com.dave.pokemon.filter.JWTTokenValidatorFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.Collections;
 
 
@@ -28,39 +19,28 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         http
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .cors().configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setExposedHeaders(Arrays.asList("Authorization"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
+                .cors().configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
                 }).and()
-                .csrf()
-                    .disable()
-                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
-                    .antMatchers("/api/v1/pokemon").authenticated()
+                    .antMatchers("/api/v1/pokemon").hasRole("USER")
                     .antMatchers("/api/v1/trainer").authenticated()
                     .antMatchers("/api/v1/battle").hasRole("ROOT")
                     .antMatchers("/api/v1/notices").permitAll()
                     .antMatchers("/api/v1/login").permitAll()
-                .and()
-                    .formLogin().and().httpBasic();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                .and().csrf().disable()
+                    .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
 }
 
